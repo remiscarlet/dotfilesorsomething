72e777127198e302b3483e0c217c25f9fb6bfa25
remiscarlet <remilia@scarl.et>
2021-10-19 17:52:08 -0400

    Store devdir in a var

--- inc/bash.aliases.dots.sh
+++ inc/bash.aliases.dots.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+DEVELOPMENT_DIR="~/Documents/development"
 
 alias docker-compose-test="docker-compose -f docker-compose.test.yml"
 alias mongosh-td="mongosh --port=27052 --username=docker --password=$(get_secret mongocreds docker)"
 
-alias cddotfiles="cd ~/Documents/development/dotfilesorsomething"
-alias cdsocial="cd ~/Documents/development/dots-microservices-social"
-alias cdcommon="cd ~/Documents/development/dots-microservices-common"
+alias cddotfiles="cd ${DEVELOPMENT_DIR}/dotfilesorsomething"
+alias cdsocial="cd ${DEVELOPMENT_DIR}/dots-microservices-social"
+alias cdcommon="cd ${DEVELOPMENT_DIR}/dots-microservices-common"
+alias cdproto="cd ${DEVELOPMENT_DIR}/protobufs"
