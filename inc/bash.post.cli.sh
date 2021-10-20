8b20d206153792baa8997800ff9d5634a2299834
remiscarlet <remilia@scarl.et>
2021-10-20 12:56:25 -0400

    Add fzf autocompletes

--- inc/bash.post.cli.sh
+++ inc/bash.post.cli.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Should come after gitprompt
+# FZF
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
+# Terminal Styling
+# Should come after gitprompt
 alias ls='ls -GFh'
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
