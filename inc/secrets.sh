3fa4a2b1da946865a7ad9d00c30bb75e4d88b30c
remiscarlet <remilia@scarl.et>
2021-12-22 06:17:25 +0000

    Remove hardcoded shellconfig path

--- inc/secrets.sh
+++ inc/secrets.sh
@@ -4,10 +4,9 @@
 # Config #
 ##########
 
-SHELL_CONFIG="/Users/ytakamoto/.bashrc" # Don't use ~ (homedir)
 SECRETS_DIR="secrets" # Dir name that contains the lookup array definition files
 
-###########
+#
 SECRETS_LOADED=1
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 S_LOOKUP_TABLE_DIR="${SCRIPT_DIR}/${SECRETS_DIR}/"
@@ -20,16 +19,6 @@ function load_secrets {
     S_TABLES=($(find $S_LOOKUP_TABLE_DIR -maxdepth 1 -mindepth 1 -type f | xargs -L1 -I% bash -c "basename %"))
 }
 load_secrets
-
-function reload_secrets {
-    unset SECRETS_LOADED
-    load_secrets
-    source ${SHELL_CONFIG}
-}
-alias reload_shell=reload_secrets
-
-# TODO:
-# - SSH keys are annoying to manage across devices. Add to secrets with mapping to actual files? How to make system independent? Or rather dependent...
 
 function get_secret {
     if [[ ! -f "${S_LOOKUP_TABLE_DIR}${1}" ]]; then
@@ -48,3 +37,11 @@ function get_secret {
     fi
     )
 }
+
+SHELL_CONFIG="$(get_secret homedirs $(get_hwid))/.bashrc"
+function reload_secrets {
+    unset SECRETS_LOADED
+    load_secrets
+    source ${SHELL_CONFIG}
+}
+alias reload_shell=reload_secrets
